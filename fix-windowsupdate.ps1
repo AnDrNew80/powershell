05a7241abcc65717617f5243d05fe01ba2ba4a33
AnDrNew80 <andrzej.drab@gmail.com>
2025-07-25 08:46:19 +0200

    Update fix-windowsupdate.ps1

--- fix-windowsupdate.ps1
+++ fix-windowsupdate.ps1
@@ -1,29 +1,3 @@
-Naprawa błędu Windows Update 
-
-Masz w Windows Update jedną, wkurzającą paczkę, która wciąż wraca i ma status „Install error …" U mnie tak właśnie wisiała jedna paczka z kodem błędu – 0x80070103”
-Spokojnie. Poniżej pokaże Ci jak w prosty sposób za pomocą gotowego  skryptu  PowerShell,mozna to posprzątać.
-
-Co to w ogóle za błąd 0x80070103?
-Najczęściej to sterownik, który:
-	• już masz w nowszej wersji, albo
-	• nie pasuje do twojego sprzętu.
-Windows próbuje go wgrać,ale się  nie udaje się, i tak w kółko.
-
-Co zrobimy?
-	1. Zatrzymamy usługi Windows Update.
-	2. Wyczyścimy wpisy w rejestrze, które mówią „hej, mam coś do dokończenia”.
-	3. Usuniemy pliki z kolejki aktualizacji.
-	4. Włączymy wszystko z powrotem.
-	5. Zrestartujesz komputer.
-Po tym Windows zwykle „zapomina” o złej paczce.
-
-Zanim zaczniesz
-	• Uruchom PowerShell jako Administrator.
-Wyszukaj „PowerShell”, kliknij prawym → Uruchom jako administrator.
-	• Zapisz skrypt do pliku, np. fix-windowsupdate.ps1.
-
- 👉 Gotowy skrypt , znajdziesz również w moim repo na Github: tutaj
-
 # Naprawa błędu windows update – z działającym paskiem postępu
 
 $script:step = 1
@@ -75,25 +49,4 @@ Start-Service msiserver
 Show-Step "Zakończono czyszczenie! Zrestartuj komputer, aby zakończyć operację."
 
 Write-Host "`n✅ Gotowe. Uruchom ponownie komputer, aby Windows Update się odświeżył." -ForegroundColor Green
-
-Ile to trwa?
-Zwykle 30–90 sekund. Najdłużej trwa czyszczenie folderów, jeśli masz dużo plików.
-
-Zablokuj sterowniki z Windows Update
-
-Jeśli nie chcesz, by Windows w ogóle ciągnął sterowniki przez Windows Update:
-
-New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" `
-  -Name ExcludeWUDriversInQualityUpdate -Value 1 -PropertyType DWord -Force
-(Wrócić do domyślnego możesz ustawiając wartość na 0 lub usuwając ten wpis.)
-
-Najczęstsze pytania (FAQ)
-❓ Czy ten skrypt coś popsuje?
-Nie powinien. On tylko czyści kolejkę aktualizacji i „flagi” w rejestrze, które mówią systemowi, że coś jest do dokończenia.
-❓ Zniknie historia aktualizacji?
-Tak, część historii może zniknąć z widoku Windows Update. To normalne po czyszczeniu.
-❓ Muszę robić restart?
-Tak. To ważne, żeby system poukładał wszystko na nowo.
-❓ Czy muszę uruchamiać PowerShell jako administrator?
-Tak. Bez tego skrypt nie ma dostępu do usług i rejestru.
 
